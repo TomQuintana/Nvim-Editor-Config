aaaa7b20343242a03e9f72b4bfcc1fac147284c1
tomasquintana-gj <tomas.quintana@gojiraf.com>
2023-07-26 12:47:38 -0300

    change TS icon

--- lua/milo/plugs/icons.lua
+++ lua/milo/plugs/icons.lua
@@ -52,12 +52,12 @@ require("nvim-web-devicons").set_icon {
     cterm_color = "65",
     name = "Makefile"
   },
-  -- ["ts"] = {
-  --   icon = "ﯤ",
-  --   color = "#519aba",
-  --   cterm_color = "65",
-  --   name = "TS"
-  -- },
+  ["ts"] = {
+    icon = "ﯤ",
+    color = "#519aba",
+    cterm_color = "65",
+    name = "TS"
+  },
 }
 
 require("nvim-web-devicons").set_default_icon('', '#41535b')
@@ -65,4 +65,4 @@ require("nvim-web-devicons").set_default_icon('', '#7a0d21')
 require("nvim-web-devicons").set_default_icon('', '#e37933')
 require("nvim-web-devicons").set_default_icon('js', '#cbcb41')
 require("nvim-web-devicons").set_default_icon('', '#6d8086')
---require("nvim-web-devicons").set_default_icon(' ', '#519aba')
+require("nvim-web-devicons").set_default_icon(' ', '#519aba')
