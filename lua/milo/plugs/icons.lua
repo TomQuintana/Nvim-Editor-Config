4e612006981c97ec4ee626638a3536d60d6e5ca9
tomQuintana <tomquintana20@gmail.com>
2023-09-20 17:16:49 -0300

    style(icons): remove color icos of tsx

--- lua/milo/plugs/icons.lua
+++ lua/milo/plugs/icons.lua
@@ -58,12 +58,6 @@ require("nvim-web-devicons").set_icon {
     cterm_color = "65",
     name = "TS"
   },
-  ["tsx"] = {
-    icon = "󰜈",
-    color = "#4E8FFA",
-    cterm_color = "65",
-    name = "TypeScriptReactSpec"
-  },
   ["sol"] = {
     icon = "",
     color = "#515D70",
@@ -79,4 +73,6 @@ require("nvim-web-devicons").set_default_icon('js', '#cbcb41')
 require("nvim-web-devicons").set_default_icon('', '#6d8086')
 require("nvim-web-devicons").set_default_icon(' ', '#519aba')
 require("nvim-web-devicons").set_default_icon('󰞻', '#519aba')
-require("nvim-web-devicons").set_default_icon('', '#519aba')
+require("nvim-web-devicons").set_default_icon('', '#519aba')
+--require("nvim-web-devicons").set_default_icon('', '#519aba')
+
