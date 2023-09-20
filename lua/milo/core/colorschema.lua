50254b06fe89dea84feb57f595cd80de53bda20d
tomQuintana <tomquintana20@gmail.com>
2023-09-20 17:09:07 -0300

    style(colorschema): change some style of colorschema options

--- lua/milo/core/colorschema.lua
+++ lua/milo/core/colorschema.lua
@@ -1,10 +1,4 @@
---local status, _ = pcall(vim.cmd, "colorscheme vscode")
 local status, _ = pcall(vim.cmd, "colorscheme tccs")
---local status, _ = pcall(vim.cmd, "colorscheme codedark")
---local status, _ = pcall(vim.cmd, "colorscheme catppuccin-mocha")
---vim.cmd[[colorscheme tokyonight-night]]
--- Lua
---vim.cmd[[colorscheme vscode]]
 
 if not status then
   print("Colorscheme not found!") -- print error if colorscheme not installed
@@ -14,21 +8,8 @@ end
 vim.opt.termguicolors = true
 vim.o.background = "dark"
 
--- -- Colors for theme mocha
--- vim.cmd [[highlight @variable guifg=#89b4fa ]] -- {req, res}
--- vim.cmd [[highlight @operator guifg=#89b4fa ]] -- {req, res}
--- vim.cmd [[highlight Function guifg=#f9e2af ]] -- amarillo
--- vim.cmd [[highlight @method guifg=#f9e2af ]] -- amarillo
--- vim.cmd [[highlight @Field guifg=#9cdefe]] -- celeste
--- vim.cmd [[highlight @Property guifg=#9cdefe]] -- res.json({@Property})
--- vim.cmd [[highlight @Parameter guifg=#9cdefe]] --Ej: console
--- --vim.cmd [[highlight @namespace guifg=#fab387]] --Ej: nombre de import 
--- vim.cmd [[highlight Structure guifg=#4ec9b0]]
--- vim.cmd [[highlight rainbowcol1 guifg=#f9e2af]]
--- vim.cmd [[highlight rainbowcol3 guifg=#f38ba8]]
---
--- Colors for vsc
 vim.cmd [[highlight @variable guifg=#9cdefe]] -- celeste
+vim.cmd [[highlight @variable.builtin guifg=#9cdefe]] -- celeste
 --vim.cmd [[highlight @string guifg=#9cdefe]] -- celeste
 --vim.cmd [[highlight @Keyword guifg=#f7a7e9]] -- await const 
 vim.cmd [[highlight @conditional guifg=#f7a7e9]] -- if 
@@ -40,29 +21,23 @@ vim.cmd [[highlight Structure guifg=#4ec9b0]] --Ej: Usuario.findById
 vim.cmd [[highlight rainbowcol1 guifg=#f9e2af]]
 vim.cmd [[highlight rainbowcol3 guifg=#f38ba8]]
 
-
--- Colors of TODO comment
-vim.cmd [[highlight TodofgTODO guifg=#f9e2af ]]
-vim.cmd [[highlight TodoBgTODO gui=bold guifg=#000000 guibg=#f9e2af ]]
-vim.cmd [[highlight TodoSignTODO guifg=#f9e2af]]
-
 vim.cmd [[highlight @operator guifg=#89b4fa ]] -- {req, res}
-
--- Colors of QUESTION comment
-vim.cmd [[highlight TodofgQUESTION guifg=#FB892F ]]
-vim.cmd [[highlight TodoBgQUESTION gui=bold guifg=#F3F3F3 guibg=#FB892F ]]
-vim.cmd [[highlight TodoSignQUESTION guifg=#FB892F]]
 
 --change color of diagnostic icon error
 vim.cmd [[highlight DiagnosticSignError guifg=#E84153 ]]
 
 -- Folder of nerdtree
 vim.cmd [[highlight NvimTreeFolderIcon guifg=#E5BE30]]
-vim.cmd [[highlight NvimTreeIndentMarker guifg=#c7d0d7]]
+vim.cmd [[highlight NvimTreeIndentMarker guifg=#E5BE30]]
 vim.cmd [[highlight NvimTreeCursorLine guifg=#8FECFC]]
 
+
+--vim.cmd [[highlight NvimTreeNormal guifg=#8FECFC]]
+--
+-- Line Color
 vim.cmd [[highlight LineNr guifg=#CCC5C5]]
 
+-- Diagnostic Highlight
 vim.cmd [[highlight BufferLineInfoDiagnosticSelected guifg=#FB892F ]]
 vim.cmd [[highlight BufferLineInfoSelected guifg=#FB892F ]]
 
@@ -72,3 +47,7 @@ vim.cmd [[highlight BufferLineHintSelected guifg=#FB892F ]]
 vim.cmd [[highlight NvimTreeLspDiagnosticsHint guifg=#FB892F ]]
 vim.cmd [[highlight DiagnosticSignHint guifg=#FB892F ]]
 vim.cmd [[highlight SagaText guifg=#FB892F ]]
+
+vim.cmd[[highlight Visual guifg=#FAF020]]
+
+
