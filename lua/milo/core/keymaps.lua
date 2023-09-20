cc14027c65ca79e579b84e45e72ef151f5bca330
tomQuintana <tomquintana20@gmail.com>
2023-09-20 17:03:01 -0300

    feat(keymaps): add keympas for lsp saga

--- lua/milo/core/keymaps.lua
+++ lua/milo/core/keymaps.lua
@@ -8,9 +8,6 @@ keymap.set('n', '<leader>q', ':q<CR>',{noremap = true})
 keymap.set('n', '<leader>wq', ':wq<CR>',{noremap = true})
 keymap.set('n', '<leader>fw', ':w!<CR>',{noremap = true})
 
--- use jk to exit insert mode
-keymap.set("i", "jk", "<ESC>")
-
 -- Telescope
 local builtin = require('telescope.builtin')
 keymap.set('n', '<leader>ff', builtin.find_files, {})
@@ -47,18 +44,23 @@ keymap.set("n", "<leader>td", ":TodoLocList:<CR>")
 -- Git
 keymap.set("n", "<leader>dp", ":VGit project_diff_preview<CR>")
 keymap.set("n", "<leader>df", ":VGit buffer_diff_preview<CR>")
+keymap.set("n", "<leader>du", ":VGit hunk_up<CR>")
+keymap.set("n", "<leader>dd", ":VGit hunk_down<CR>")
 
 -- Python
 vim.api.nvim_set_keymap('n', '<leader>d', '<Cmd>lua require("jedi").goto()<CR>', { noremap = true, silent = true })
 
 keymap.set("n", "<leader>gd", ":Lspsaga goto_definition<CR>")
---keymap.set("n", "<leader>gi", ":Lspsaga goto_type_definition<CR>")
+keymap.set("n", "<leader>gi", ":Lspsaga goto_type_definition<CR>")
 keymap.set("n", "<leader>hd", ":Lspsaga hover_doc<CR>")
 keymap.set("n", "<leader>gf", ":Lspsaga finder<CR>")
 keymap.set("n", "<leader>ca", ":Lspsaga code_action<CR>")
 keymap.set("n", "<leader>d", ":Lspsaga show_cursor_diagnostics<CR>")
-keymap.set("n", "[d", ":Lspsaga diagnostic_jump_prev<CR>")
+keymap.set("n", "ne", ":Lspsaga diagnostic_jump_prev<CR>")
+keymap.set("n", "pe", ":Lspsaga diagnostic_jump_next<CR>")
+keymap.set("n", "pf", "<cmd>Lspsaga peek_definition<CR>") 
 
+--NOTE:
 --Ctrl O to jump back to the previous (older) location.
 --Ctrl I same as Tab) to jump forward to the next (newer) location
 
@@ -67,5 +69,3 @@ keymap.set("n", "<leader>vs", "<C-w>v") -- split window vertically
 keymap.set("n", "<leader>hs", "<C-w>s") -- split window horizontally
 keymap.set("n", "<leader>es", "<C-w>=") -- make split windows equal width & height
 keymap.set("n", "<leader>xs", ":close<CR>") -- close current split window
-
-
